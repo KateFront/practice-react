a397a9737812cc831e20d197ec1643766b675b74
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-04-27 10:06:06 +0300

    add title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            Lesson-8
+        </div>
+    );
 }
 
 export default App;
