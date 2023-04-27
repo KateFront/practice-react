5823da98cb5ff324c759257e951874a38485b86f
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-04-27 13:02:03 +0300

    add state and props in the component

---
+++ src/components/RandomNumber.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import generateRandomNum from "../utils/generateRandomNum";
+
+
+const RandomNumber = ({maxNum}) => {
+
+    const [randomNum, setRandomNum] = useState(generateRandomNum(maxNum));
+    const changeRandomNum = () => {
+        setRandomNum(generateRandomNum(maxNum))
+    }
+    return (
+        <div>
+            <h1>{randomNum}</h1>
+            <button onClick={changeRandomNum}>Generate new random number</button>
+        </div>
+    );
+};
+
+export default RandomNumber;
