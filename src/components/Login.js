9182d2708758d2736d0dbf89fe508607560d3fab
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-04-28 11:29:56 +0300

    add controled input

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+    /*  const [username, setUsername] = useState('');
+      const [password, setPassword] = useState('');
+      */
+    const [data, setData] = useState({username: '', password: ''})
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        const userData = {
-            username: event.target.username.value,
-            password: event.target.password.value
-        }
-        alert(JSON.stringify(userData));
+        /*  const userData = {
+              username,
+              password
+          }*/
+        alert(JSON.stringify(data));
+    }
+
+    /* const inputUsernameChange = (e) => {
+         setData({...data, username: e.target.value})
+     }
+     const inputPasswordChange = (e) => {
+         setData({...data, password: e.target.value})
+     }*/
+
+    const handleInputChange = (e, name) => {
+        setData({...data, [name]: e.target.value})
     }
 
     return (
@@ -15,10 +31,14 @@ const Login = () => {
             <h1>Login form</h1>
             <form onSubmit={handleFormSubmit}>
                 <label>Username:
-                    <input type="text" name="username"/>
+                    <input type="text"
+                           value={data.username}
+                           onChange={(e) => handleInputChange(e, 'username')}/>
                 </label>
                 <label>Password:
-                    <input type="password" name="password"/>
+                    <input type="password"
+                           value={data.password}
+                           onChange={(e) => handleInputChange(e, 'password')}/>
                 </label>
                 <button type="submit">Login</button>
             </form>
