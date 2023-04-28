fa240bf87af17f009373fe90a3d66771e49ca72d
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-04-28 12:14:05 +0300

    add hook useffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    const [todo, setTodo] = useState(null);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => setTodo(json))
+    }, []);
+
+    console.log('app rendered')
+    console.log(todo)
+
+
+
+    return (
+        <div className="App">
+            {todo &&
+                <h1>{todo.title}</h1>
+            }
+        </div>
+    );
 }
 
 export default App;
